78b0a08beeccb7236a47c047cac3c0cb9b7d84ce
maxim <maxim@paymoapp.com>
2025-05-03 14:10:06 +0300

    add delete file

--- src/app/f/[folderId]/file-row.tsx
+++ src/app/f/[folderId]/file-row.tsx
@@ -1,6 +1,8 @@
-import { FileIcon, Folder as FolderIcon } from "lucide-react";
+import { FileIcon, Folder as FolderIcon, Trash2Icon } from "lucide-react";
 import Link from "next/link";
 import type { files_table, folders_table } from "~/server/db/schema";
+import { Button } from "~/components/ui/button";
+import { deleteFile } from "~/server/actions";
 
 export function FileRow(props: { file: typeof files_table.$inferSelect }) {
   const { file } = props;
@@ -20,8 +22,15 @@ export function FileRow(props: { file: typeof files_table.$inferSelect }) {
             {file.name}
           </a>
         </div>
-        <div className="col-span-3 text-gray-400">{"File"}</div>
+        <div className="col-span-2 text-gray-400">{"File"}</div>
         <div className="col-span-3 text-gray-400">{file.size}</div>
+        <div className="col-span-1 text-gray-400">
+          <Button variant="ghost" size="icon" onClick={() => {
+            deleteFile(file.id);
+          }} aria-label="Delete file">
+            <Trash2Icon className="w-4 h-4" />
+          </Button>
+        </div>
       </div>
     </li>
   );
---
+++ src/server/actions.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { and, eq } from "drizzle-orm";
+import { db } from "./db";
+import { files_table } from "./db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { UTApi } from "uploadthing/server";
+import { cookies } from "next/headers";
+
+const utApi = new UTApi();
+
+export async function deleteFile(fileId: number) {
+    const session = await auth();
+
+    if (!session.userId) {
+        throw {error: "Unauthorized"};
+    } 
+
+    const [file] = await db
+      .select()
+      .from(files_table)
+      .where(
+        and(eq(files_table.id, fileId), eq(files_table.ownerId, session.userId))
+      );
+
+    if (!file) {
+        return { error: "File not found" };
+    }
+  
+    const utResponse = await utApi.deleteFiles([file.url.replace("https://utfs.io/f/", "")]);
+
+    if (!utResponse.success) {
+        return { error: "Failed to delete file from UploadThing" };
+    }
+
+    console.log(utResponse);
+
+    const dbResult = await db.delete(files_table).where(eq(files_table.id, fileId));
+
+    const c = await cookies();
+
+    c.set("force-refresh", JSON.stringify(Math.random()));
+
+    return { success: true };
+}
